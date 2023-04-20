58b813d49151629504bb3bcc4de463f40ee3716b
jia-bing-xin <441026068@qq.com>
2023-04-20 14:07:23 +0800

    创建项目

---
+++ scripts/bundle.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm -rf ./dist
+
+mkdir ./dist
+mkdir ./dist/subapp
+
+# sub-react子应用
+cp -r ./sub-react/build/ ./dist/subapp/sub-react/
+
+# sub-vue子应用
+cp -r ./sub-vue3/dist/ ./dist/subapp/sub-vue3/
+
+# main基座
+cp -r ./main/dist/ ./dist/main/
+
+# cd ./dist
+# zip -r mp$(date +%Y%m%d%H%M%S).zip *
+# cd ..
+echo 'bundle.sh execute success.'
